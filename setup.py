9ce0477067492c8282aafb1bc43417e83194082c
Yuhan-Kunlun <yuhan-kunlun@Yuhan-KunlundeMacBook-Pro.local>
2024-03-04 11:43:30 +0800

    refactor

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="pyspring",
+    name="pyspring-inject",
     version="0.0.1",
     author="Yuhan Wei",
     author_email="weiyuhan@pku.edu.cn",
@@ -13,9 +13,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     install_requires=[
-        'pyhocon==0.3.60',
+        'pyhocon>=0.3.60',
         'inject==5.0.0',
     ]
 )
